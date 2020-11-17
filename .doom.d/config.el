a9de1933472c624abf6d43d5d602e976159b97d0
Zachary Dodge <zacharysdodge@gmail.com>
2020-11-17 11:34:58 -0800

    Adjust js indent size and theme

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -27,7 +27,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-solarized-light)
+(setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -37,6 +37,7 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+(setq js-indent-level 2)
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,8 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" default))
- '(package-selected-packages '(w3m org-plus-contrib)))
+   '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" default))
+ '(package-selected-packages '(smooth-scrolling w3m org-plus-contrib)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
