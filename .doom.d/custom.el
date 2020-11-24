138e482d791cbbf4eb7c23c5b80642f21ef8f46a
Zachary Dodge <zacharysdodge@gmail.com>
2020-11-24 12:42:50 -0800

    Add lua support

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" default))
+ '(lua-indent-level 4)
  '(package-selected-packages '(smooth-scrolling w3m org-plus-contrib))
  '(web-mode-code-indent-offset 2))
 (custom-set-faces
