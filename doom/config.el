3dfc6342baf1f64692a7d83171aa4806dbb7f0c0
Zachary Dodge <zacharysdodge@gmail.com>
2021-10-10 11:38:15 -0700

    Cleanup emacs config.org and add minimap/treemacs

--- doom/config.el
+++ doom/config.el
@@ -1,19 +1,34 @@
 (setq user-full-name "Zachary Dodge"
       user-mail-address "zacharysdodge@gmail.com")
+
 (setq doom-theme 'doom-dracula)
+(setq doom-font (font-spec :family "Hack Nerd Font Mono" :size 16))
+
 (setq org-support-shift-select t)
+
 (setq display-line-numbers-type t)
 (setq projectile-indexing-method 'alien)
+(blink-cursor-mode)
+(setq confirm-kill-emacs nil)
+(setq mouse-wheel-progressive-speed nil)
+
 (defun centaur-tabs-buffer-groups ()
   (list
     (cond
       ((string-equal "*" (substring (buffer-name) 0 1)) "Emacs")
       (t "User"))))
-(setq doom-font (font-spec :family "Hack Nerd Font Mono" :size 16))
-(show-smartparens-global-mode)
-(blink-cursor-mode)
-(setq confirm-kill-emacs nil)
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-(magit-delta-mode)
 (use-package! magit-delta
   :hook (magit-mode . magit-delta-mode))
+(use-package! minimap
+  :config
+  (minimap-mode))
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(use-package! treemacs
+  :config
+  (defun project-treemacs ()
+    (if (and (doom-project-p) (not (equal (treemacs-current-visibility) 'visible)))
+      (treemacs)
+      t))
+  (add-hook! prog-mode project-treemacs))
