722b702851c28601ec8453a72b9d2efc5f7f5921
Zachary Dodge <zacharysdodge@gmail.com>
2021-10-19 07:28:14 -0700

    emacs - fix centaur-tabs and wsl frame size

--- doom/config.el
+++ doom/config.el
@@ -17,9 +17,10 @@
    (cond
     ((string-equal "*" (substring (buffer-name) 0 1)) "Emacs")
     (t "User"))))
-(use-package! centaur-tabs
-  :config
-  (setq centaur-tabs-buffer-groups-function #'custom-centaur-tabs-buffer-groups))
+(when (not (string-equal system-type "windows-nt"))
+  (use-package! centaur-tabs
+    :config
+    (setq centaur-tabs-buffer-groups-function #'custom-centaur-tabs-buffer-groups)))
 (use-package! magit-delta
   :hook (magit-mode . magit-delta-mode))
 
@@ -42,14 +43,14 @@
       (`(,display-x ,display-y ,display-raw-width ,display-raw-height)
        (let* ((proportion (or proportion 0.9))
               (margin (/ (- 1 proportion) 2))
-              (display-width (- display-raw-width display-x))
+              (display-width (- display-raw-width (if (< display-x 1920) display-x 0)))
               (display-height (- display-raw-height display-y))
               (width (truncate (* display-width proportion)))
               (height (truncate (* display-height proportion)))
               (margin-left (+ display-x (truncate (* display-width margin))))
               (margin-top (+ display-y (truncate (* display-height margin)))))
-         (set-frame-size (selected-frame) width height t)
-         (set-frame-position (selected-frame) margin-left margin-top))))))
+         (set-frame-position (selected-frame) margin-left margin-top)
+         (set-frame-size (selected-frame) width height t))))))
 (set-ideal-frame-size)
 (map! :leader
       :desc "Set ideal frame size" "F" #'set-ideal-frame-size)
