cbb3da121f6fc9253be40194348acd309644555d
Zachary Dodge <zacharysdodge@gmail.com>
2021-11-05 16:52:29 -0700

    emacs - refactor: switch to hand-rolled config

---
+++ emacs/init.el
@@ -0,0 +1,131 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(defun display-startup-time ()
+  "Displays the startup time in the echo buffer when Emacs is finished loading."
+  (message "Emacs loaded in %s with %d garbage collections."
+	   (format "%.2f seconds"
+		   (float-time (time-subtract after-init-time before-init-time)))
+	   gcs-done))
+(add-hook 'emacs-startup-hook #'display-startup-time)
+
+(defun set-ideal-frame-size (&optional proportion)
+  (interactive)
+  (when (display-graphic-p)
+    (pcase (frame-monitor-workarea)
+      (`(,display-x ,display-y ,display-raw-width ,display-raw-height)
+       (let* ((proportion (or proportion 0.9))
+              (margin (/ (- 1 proportion) 2))
+              (display-width (- display-raw-width (if (< display-x 1920) display-x 0)))
+              (display-height (- display-raw-height display-y))
+              (width (truncate (* display-width proportion)))
+              (height (truncate (* display-height proportion)))
+              (margin-left (+ display-x (truncate (* display-width margin))))
+              (margin-top (+ display-y (truncate (* display-height margin)))))
+         (set-frame-position (selected-frame) margin-left margin-top)
+         (set-frame-size (selected-frame) width height t))))))
+(add-hook 'emacs-startup-hook #'set-ideal-frame-size)
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+(use-package ivy
+  :config
+  (ivy-mode))
+
+(use-package no-littering
+  :config
+  (setq auto-save-file-name-transforms
+	`((".*" ,(no-littering-expand-var-file-name "auto-save/") t))))
+
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
+(use-package all-the-icons)
+
+(use-package rainbow-delimiters
+	:hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package ws-butler
+	:hook ((text-mode . ws-butler-mode)
+				 (prog-mode . ws-butler-mode)))
+
+(use-package projectile)
+
+(use-package popper
+  :after projectile
+  :init
+  (setq popper-reference-buffers
+        '("\\*Messages\\*"
+          "^\\*Warnings\\*"
+          "^\\*IBuffer\\*"
+          "^\\*Compile-Log\\*"
+          "^\\*Backtrace\\*"
+          "[Oo]utput\\*$"
+          "\\*Help\\*"
+          "\\*helpful\\*"
+          "\\*vterm\\*"
+          "\\*Excorporate\\*"
+          "\\*xref\\*"
+          help-mode
+          helpful-mode
+          compilation-mode
+          term-mode
+          vterm-mode)
+        popper-group-function #'popper-group-by-projectile)
+  (popper-mode t))
+
+(use-package which-key
+	:config
+	(which-key-mode)
+	(setq which-key-idle-delay 0.3))
+
+(use-package evil
+	:config
+	(evil-mode t)
+	(evil-global-set-key 'motion "j" 'evil-next-visual-line)
+	(evil-global-set-key 'motion "k" 'evil-previous-visual-line))
+
+(use-package general)
+
+; Customization
+(setq-default
+ delete-by-moving-to-trash t
+ tab-width 2)
+
+(global-subword-mode t)
+(tool-bar-mode 0)
+(tooltip-mode 0)
+(menu-bar-mode 0)
+(set-face-attribute 'default nil :font "Hack Nerd Font Mono" :height 130)
+(column-number-mode)
+(global-display-line-numbers-mode t)
+(global-hl-line-mode)
+
+(setq global-auto-revert-non-file-buffers t)
+(global-auto-revert-mode t)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(general-define-key
+ :states 'normal
+ :keymaps 'override
+ :prefix "SPC"
+ "SPC" '(execute-extended-command :which-key "M-x")
+ "e" '(:ignore t :which-key "eval")
+ "e b" '(eval-buffer :which-key "eval-buffer"))
