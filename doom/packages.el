b7fc201b1642dd508edf86e1d63cc57748cb2eeb
Zachary Dodge <zacharysdodge@gmail.com>
2022-04-21 23:12:14 -0700

    fix(emacs): doom tweaks including undo-tree

--- doom/packages.el
+++ doom/packages.el
@@ -41,7 +41,6 @@
 ;(package! builtin-package :pin "1a2b3c4d5e")
 
 (package! magit-delta)
-(package! evil-colemak-basics)
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
