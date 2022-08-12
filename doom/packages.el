dcfce7efeae2ced76a210f0bf0f6fd47e4a03b3b
Zachary Dodge <zdodge@amazon.com>
2022-08-12 11:47:57 -0700

    feat(emacs): setup projectile rg on SPC-/

--- doom/packages.el
+++ doom/packages.el
@@ -41,6 +41,7 @@
 ;(package! builtin-package :pin "1a2b3c4d5e")
 
 ;; (package! magit-delta)
+(package! rg)
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
