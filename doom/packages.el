4c1aa886ec0452e69f5d200ed5cd2ec157900cf8
dodgez <zacharysdodge@gmail.com>
2022-10-19 09:33:33 -0700

    feat(emacs): make org mode more visually pleasant

--- doom/packages.el
+++ doom/packages.el
@@ -42,6 +42,8 @@
 
 (package! magit-delta)
 (package! rg)
+(package! org-appear)
+(package! visual-fill-column)
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
