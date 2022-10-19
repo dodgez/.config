022beca2efc6773c7533067c4d4ff0bb187b93d2
dodgez <zacharysdodge@gmail.com>
2022-10-19 09:13:22 -0700

    feat(emacs): add magit delta

--- doom/packages.el
+++ doom/packages.el
@@ -40,7 +40,7 @@
 ;; Use `:pin' to specify a particular commit to install.
 ;(package! builtin-package :pin "1a2b3c4d5e")
 
-;; (package! magit-delta)
+(package! magit-delta)
 (package! rg)
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
