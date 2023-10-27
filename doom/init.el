43538e6332341791bf14a9be85fbc1beea6d239f
Zachary Dodge <zacharysdodge@gmail.com>
2023-10-27 10:08:52 -0700

    remove(emacs): unused chatgpt module

--- doom/init.el
+++ doom/init.el
@@ -89,7 +89,6 @@
        :tools
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
-       chatgpt
        ;;collab            ; buffers with friends
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
